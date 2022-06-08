dc47c3f711774638edbd230ee077e62f8cb945c6
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-08 13:16:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2; //multiplication
+
+let random = Math.floor(Math.random() * 10);//random method
+
+let num3 = 20;
+let num4 = 5;
+
+let mod = num3 / num4; //modulus operation, returns the remainder
+
+let max = Math.max(1,2,3,4,7,9,10,19,20)  
